0fee4625019c9403c98999c24b22dffd505a8c7e
Pavel Sorejs <sorejs@gmail.com>
2021-07-02 21:28:58 +0200

    keep mode when switching ac ON

--- climate.py
+++ climate.py
@@ -253,9 +253,10 @@ class DaikinMadokaClimate(ClimateEntity):
     async def async_set_hvac_mode(self, hvac_mode):
         """Set HVAC mode."""
         try:
-            await self.controller.operation_mode.update(
-                OperationModeStatus(HA_MODE_TO_DAIKIN.get(hvac_mode))
-            )
+            if vac_mode != HVAC_MODE_OFF :
+                await self.controller.operation_mode.update(
+                    OperationModeStatus(HA_MODE_TO_DAIKIN.get(hvac_mode))
+                )
             await self.controller.power_state.update(
                 PowerStateStatus(hvac_mode != HVAC_MODE_OFF)
             )
